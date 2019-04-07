8d703fb05f60eb532590fd4a4b1f4dc71952d222
Martin Valentino <martin.david.siagian@gmail.com>
2019-04-07 11:40:47 +1000

    Refactor to use blueprints and application factory

--- services/users/manage.py
+++ services/users/manage.py
@@ -1,8 +1,13 @@
-from flask.cli import FlaskGroup
-from project import app, db
 import unittest
+from flask.cli import FlaskGroup
 
-cli = FlaskGroup(app)
+from project import create_app, db
+from project.api.models import User
+
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
 
 @cli.command('recreate_db')
 def recreate_db():
@@ -20,6 +25,7 @@ def test():
         return 0
 
     return 1
+
 
 if __name__ == '__main__':
     cli()
--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,30 +1,28 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean(), default=True, nullable=False)
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
 
-    def __init__(self, username: str, email: str):
-        self.username = username
-        self.email = email
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    # set up extensions
+    db.init_app(app)
 
-@app.route('/users/ping', methods=['GET'])
-def ping_pong():
-    return jsonify({
-        'status': 'success',
-        'message': 'pong!'
-    })
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return { 'app': app, 'db': db }
+
+    return app
