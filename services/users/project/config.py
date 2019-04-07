efaed951fe2dd68aed6ef64c640d6358fd4a1047
Martin Valentino <martin.david.siagian@gmail.com>
2019-04-07 10:43:29 +1000

    Add integration with postgresql database

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -1,19 +1,24 @@
+import os
+
 
 class BaseConfig:
     """Base Configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class DevelopmentConfig(BaseConfig):
     """Development Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     """Testing Configuration"""
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    
